e104b0c8da5623e019d14b425d093624e56d5fef
Pooja P <poojahegde2003@gmail.com>
2025-06-25 08:16:06 +0530

    Profile selector created

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input'
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector'
 const SignUp = ({setCurrentPage}) => {
   const [profilePic,setProfilePic]=useState(null);
   const [fullName,setFullName]=useState('');
@@ -14,12 +15,14 @@ const SignUp = ({setCurrentPage}) => {
     e.preventDefault();
   }
   return (
+
     <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center '>
       <h3 className='text-lg font-semibold text-black '>Create an Account</h3>
       <p className='text-xs text-slate-700 mt-[5px] mb-6'>
         Join us today by entering your details below
       </p>
       <form onSubmit={handleSignUp}>
+      <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
         <div className='grid grid-cols-1 md:grid-cols-1 gap-2'>
           <Input value={fullName} onChange={({target})=>setFullName(target.value)} label="Full Name" placeholder="John" type="text"/>
           <Input value={email} onChange={({target})=>setEmail(target.value)} label="Email Address" placeholder="example@gmail" type="text"/>
